c81a2aabf35dbdf925b5ae1c54a692c1ebceadbb
CodeSandbox <support@codesandbox.io>
2026-01-07 01:25:52 +0000

    Sandbox snapshot commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,13 @@ export default function App() {
       <h1>Weather App</h1>
       <Weather />
 
-     <footer>
-  <p>
-    Open-source code on{" "}
-    <a
-      href="https://github.com/ShaniceS13/react-weather-app"
-      target="_blank"
-    >
-      GitHub
-    </a>
-  </p>
-</footer>
-
-
-
+      <footer>
+        <p>
+          This project was coded by <a href="https://github.com/ShaniceS13" target="_blank" rel="noreferrer">Shanice Solomon</a>  and is open-sourced on 
+        <a href="https://github.com/ShaniceS13/react-weather-app" target="_blank" rel="noreferrer"> GitHub </a> 
+        and is hosted on <a href="https://whimsical-biscochitos-5fa554.netlify.app/" target="_blank" rel="noreferrer">Netlify</a> 
+        </p>
+      </footer>
     </div>
   );
 }
